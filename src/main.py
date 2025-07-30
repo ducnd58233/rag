2c2b994258539369ef93dc230baee98228e12c47
ducnd58233 <only4wns@gmail.com>
2025-07-31 02:51:24 +0700

    fix: fix search

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from .file_loader import load_and_split
 from .rag import RAGParams, RAGSystem
 
 if __name__ == "__main__":
-    rag = RAGSystem(
+    rag1 = RAGSystem(
         RAGParams(
             llm_model=settings.rag.llm_model,
             embedding_model=settings.rag.embedding_model,
@@ -19,24 +19,32 @@ if __name__ == "__main__":
             "tags": ["tax", "income", "regulation"],
         },
     )
-    rag.add_documents(documents)
+    rag1.add_documents(documents)
 
-    result1 = rag.search(
+    result1 = rag1.search(
         query="tiền thưởng kèm theo kỷ niệm chương được tính như thế nào",
         metadata_filter={
             "department": "finance",
             "year": 2013,
         },
-        score_threshold=0.5,
+        score_threshold=0.1,
     )
 
-    result2 = rag.search(
+    rag2 = RAGSystem(
+        RAGParams(
+            llm_model=settings.rag.llm_model,
+            embedding_model=settings.rag.embedding_model,
+            collection_name=settings.rag.collection_name,
+            llm_api_key=settings.rag.openrouter_key,
+        )
+    )
+    result2 = rag2.search(
         query="thông tin các khoản chịu thuế",
         metadata_filter={
             "department": "finance",
             "year": {"lte": 2010},
         },
-        score_threshold=0.5,
+        score_threshold=0.1,
     )
 
     print("RAG result 1: ", result1["result"].content)
